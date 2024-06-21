f1d99020d7fc2aedcb206910ba8a519f2fb1f0c9
Naveen Anil <naveen.anil@wind-pioneers.com>
2024-06-21 19:15:04 +0530

    OPS: gitignore igit fodler

--- infragit/__main__.py
+++ infragit/__main__.py
@@ -1,12 +1,20 @@
+import os
 from argparse import ArgumentParser
 
 
 def main():
     parser = ArgumentParser()
     parser.add_argument("command", nargs=1)
+    parser.add_argument("--directory", "-d", type=str, required=False)
 
     args = parser.parse_args()
-    print(args.command)
+
+    command = args.command[0]
+    directory = os.getcwd() if not args.directory else os.path.abspath(os.path.join(os.getcwd(), args.directory))
+
+    if command == "init":
+        print("Creating folder:", os.path.join(directory, ".igit"))
+        os.makedirs(os.path.join(directory, ".igit"), exist_ok=True)
 
 
 if __name__=="__main__":
